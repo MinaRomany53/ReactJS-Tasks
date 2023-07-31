ad128b8bdd02d314fcac26dcfab2d10606150ada
MinaRomany53 <minaromany190@gmail.com>
2023-07-31 08:55:05 +0300

    Fetching Questions from fake API  -  Build Start Quiz Screen

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,54 @@
-import DateCounter from "./components/DateCounter";
+import { useState, useEffect, useRef, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
 
 export default function App() {
+  const [isStarted, setIsStarted] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const noQuestions = questions.length;
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const response = await fetch("http://localhost:9000/questions");
+        const data = await response.json();
+        console.log("data: ", data);
+
+        setQuestions(data);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {isLoading ? (
+          <Loader />
+        ) : isError ? (
+          <Error />
+        ) : !isStarted ? (
+          <StartScreen
+            setIsStarted={() => setIsStarted(!isStarted)}
+            noQuestions={noQuestions}
+          />
+        ) : (
+          <div>Quiz has been Started</div>
+        )}
+      </Main>
     </div>
   );
 }
